e8f2213ff1eab70d03e17f4f5ef2b4b60d364aa3
Kruzya <admin@crazyhackgut.ru>
2016-10-23 17:28:51 +0400

    Replaced <br /> to PHP_EOL in sb_debug_connection.php

--- web_upload/sb_debug_connection.php
+++ web_upload/sb_debug_connection.php
@@ -44,39 +44,39 @@ header("Content-Type: text/plain");
 if(empty($serverip) || empty($serverport))
 	die('[-] Не указана информация о сервере. Откройте текстовым редактором этот файл, пропишите в нём IP и порт, сохраните и загрузите обратно на сервер.');
 
-echo '[+] SourceBans "DebugConnection()" запущен для сервера ' . $serverip . ':' . $serverport . '<br /><br />';
+echo '[+] SourceBans "DebugConnection()" запущен для сервера ' . $serverip . ':' . $serverport . "\n\n";
 
 // Попытаемся установить соединение
-echo '[+] Открываю UDP-сокет...<br />';
+echo '[+] Открываю UDP-сокет...'.PHP_EOL;
 $sock = @fsockopen("udp://" . $serverip, $serverport, $errno, $errstr, 2);
 
 $isBanned = false;
 
 if(!$sock)
-    echo '[-] Ошибка соединения. #' . $errno . ': ' . $errstr . '<br />';
+    echo '[-] Ошибка соединения. #' . $errno . ': ' . $errstr . PHP_EOL;
 else {
-    echo '[+] UDP-соединение успешно установлено!<br />';
+    echo '[+] UDP-соединение успешно установлено!'.PHP_EOL;
 
     stream_set_timeout($sock, 1);
 
     // Попытаемся получить информацию у сервера
-    echo '[+] Записываю запрос в сокет...<br />';
+    echo '[+] Записываю запрос в сокет..'.PHP_EOL;
     if(fwrite($sock, "\xFF\xFF\xFF\xFF\x54Source Engine Query\0") === false)
-        echo '[-] Ошибка записи.<br />';
+        echo '[-] Ошибка записи.'.PHP_EOL;
     else {
-        echo '[+] Запрос успешно записан в сокет. (Это не означает, что с соединением всё в порядке.) Читаю ответ...<br />';
+        echo '[+] Запрос успешно записан в сокет. (Это не означает, что с соединением всё в порядке.) Читаю ответ...'.PHP_EOL;
         $packet = fread($sock, 1480);
 
         if(empty($packet))
-            echo '[-] Ошибка при получении информации о сервере. Не удаётся прочитать UDP-соединение. Порт заблокирован.<br />';
+            echo '[-] Ошибка при получении информации о сервере. Не удаётся прочитать UDP-соединение. Порт заблокирован.'.PHP_EOL;
         else {
             if(substr($packet, 5, (strpos(substr($packet, 5), "\0")-1)) == "Banned by server") {
-                printf('[-] Ответ получен, но веб-сервер заблокирован. Удалите блокировку с сервера (removeip %s), и повторите попытку.<br />', $_SERVER['SERVER_ADDR']);
+                printf('[-] Ответ получен, но веб-сервер заблокирован. Удалите блокировку с сервера (removeip %s), и повторите попытку.%s', $_SERVER['SERVER_ADDR'], PHP_EOL);
                 $isBanned = true;
             } else {
                 $packet = substr($packet, 6);
                 $hostname = substr($packet, 0, strpos($packet, "\0"));
-                echo '[+] Ответ получен! Сервер: ' . $hostname . ' <br />';
+                echo '[+] Ответ получен! Сервер: ' . $hostname . PHP_EOL;
             }
         }
     }
@@ -86,13 +86,13 @@ else {
 echo '<br />';
 
 // Проверим на доступность и записываемость TCP-соединения
-echo '[+] Попытка установить TCP-соединение...<br />';
+echo '[+] Попытка установить TCP-соединение...'.PHP_EOL;
 $sock = @fsockopen($serverip, $serverport, $errno, $errstr, 2);
 if(!$sock)
-    echo '[-] Ошибка соединения. #' . $errno . ': ' . $errstr . '<br />';
+    echo '[-] Ошибка соединения. #' . $errno . ': ' . $errstr . PHP_EOL;
 else
 {
-    echo '[+] TCP-соединение успешно установлено!<br />';
+    echo '[+] TCP-соединение успешно установлено!'.PHP_EOL;
     if(empty($serverrcon))
         echo '[o] Прерываю работу. RCON-пароль не установлен.';
     else if($isBanned)
@@ -102,17 +102,17 @@ else
         $data = pack("VV", 0, 03) . $serverrcon . chr(0) . '' . chr(0);
         $data = pack("V", strlen($data)) . $data;
 
-        echo '[+] Пытаюсь записать в TCP-сокет и произвести авторизацию...<br />';
+        echo '[+] Пытаюсь записать в TCP-сокет и произвести авторизацию...'.PHP_EOL;
 
         if(fwrite($sock, $data, strlen($data)) === false)
-            echo '[-] Ошибка записи.<br />';
+            echo '[-] Ошибка записи.'.PHP_EOL;
         else {
-            echo '[+] Запрос авторизации успешно записан. Читаю ответ...<br />';
+            echo '[+] Запрос авторизации успешно записан. Читаю ответ...'.PHP_EOL;
             $size = fread($sock, 4);
             if(!$size)
-                echo '[-] Ошибка чтения.<br />';
+                echo '[-] Ошибка чтения.'.PHP_EOL;
             else {
-                echo '[+] Got an response! <br />';
+                echo '[+] Got an response!'.PHP_EOL;
                 $size = unpack('V1Size', $size);
                 $packet = fread($sock, $size["Size"]);
                 $size = fread($sock, 4);
@@ -120,7 +120,7 @@ else
                 $packet = fread($sock, $size["Size"]);
                 $ret = unpack("V1ID/V1Reponse/a*S1/a*S2", $packet);
                 if(empty($ret) || (isset($ret['ID']) && $ret['ID'] == -1))
-                    echo '[-] RCON-пароль задан неверный ;) Не пытайтесь и дальше производить попытки, иначе ваш веб-сервер "улетит" в бан.<br />';
+                    echo '[-] RCON-пароль задан неверный ;) Не пытайтесь и дальше производить попытки, иначе ваш веб-сервер "улетит" в бан.';
                 else
                     echo '[+] Пароль задан правильно!';
             }
