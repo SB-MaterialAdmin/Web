d9fb9afc13ac8e7878ddfbd2e49fff482deae33e
AS.^TRO <redbol1297@gmail.com>
2016-10-31 17:09:52 +0100

    Update to 1.1.5

--- web_upload/themes/new_box/theme.conf.php
+++ web_upload/themes/new_box/theme.conf.php
@@ -32,7 +32,7 @@ define('theme_name', "SourceBans Theme (DeluxeEdition)");
 define('theme_author', "ASTRO & XaH JoB & Vampir & SAZONISCHE & NoTiCE & CrazyHackGUT");
 
 // Set the version of the theme here
-define('theme_version', "1.1.4");
+define('theme_version', "1.1.5 Not Stabled(In developing)");
 
 // Set the link of the theme here
 define('theme_link', "http://hlmod.ru/threads/alpha-material-admin-refork-na-osnove-sb-1-5-4-7-bootstrap-3.36382/");
