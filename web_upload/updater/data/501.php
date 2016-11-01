c2ba9a6c54768d46c712ad1ca048b1aea285ab5f
AS.^TRO <redbol1297@gmail.com>
2016-11-01 16:21:28 +0100

    Update to update 501.php

--- web_upload/updater/data/501.php
+++ web_upload/updater/data/501.php
@@ -1,5 +1,5 @@
 <?php
-	$ret = $GLOBALS['db']->Execute("CREATE TABLE `".DB_PREFIX."_vay4er` ( `aid` int(6) NOT NULL auto_increment, `activ` int(6) NOT NULL, `value` bigint(20) NOT NULL, `days` int(11) NOT NULL, `group_web` varchar(128) NOT NULL, `group_srv` varchar(128) NOT NULL, PRIMARY KEY  (`aid`)) ENGINE='MyISAM' DEFAULT CHARSET=utf8;");
+	$ret = $GLOBALS['db']->Execute("CREATE TABLE IF NOT EXISTS `".DB_PREFIX."_vay4er` ( `aid` int(6) NOT NULL auto_increment, `activ` int(6) NOT NULL, `value` bigint(20) NOT NULL, `days` int(11) NOT NULL, `group_web` varchar(128) NOT NULL, `group_srv` varchar(128) NOT NULL, PRIMARY KEY  (`aid`)) ENGINE='MyISAM' DEFAULT CHARSET=utf8;");
 	if(!$ret)
 		return false;
 	
