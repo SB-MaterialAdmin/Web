11691d0139a16842e568cc23cb5ac6c4cce84685
AS.^TRO <redbol1297@gmail.com>
2016-11-01 13:19:05 +0100

    Fix 506.php

--- web_upload/updater/data/506.php
+++ web_upload/updater/data/506.php
@@ -1,15 +1,16 @@
 <?php
 
-	$ret = $GLOBALS['db']->Execute("ALTER TABLE `".DB_PREFIX."_admins` ADD `support` int(6) NULL DEFAULT '0';");
-	if(!$ret)
-		return false;
+	//$ret = $GLOBALS['db']->Execute("ALTER TABLE `".DB_PREFIX."_admins` ADD `support` int(6) NULL DEFAULT '0';");
+	//if(!$ret)
+	//	return false;
 		
 	$ret = $GLOBALS['db']->Execute("INSERT INTO `".DB_PREFIX."_settings` (`setting`, `value`) VALUES ('config.home.comms', '1');");
 	if(!$ret){
-		$ret12 = $GLOBALS['db']->Execute("UPDATE `".DB_PREFIX."_settings` SET `setting` = 'config.home.comms', `value` = '1' WHERE `setting` = 'config.home.comms' AND `setting` = 'config.home.comms' COLLATE utf8mb4_bin;");
-		if(!$ret12){
-			return false;
-		}
+		//$ret12 = $GLOBALS['db']->Execute("UPDATE `".DB_PREFIX."_settings` SET `value` = '1' WHERE `setting` = 'config.home.comms' COLLATE utf8mb4_bin;");
+		//if(!$ret12){
+		//	return false;
+		//}
+		return false;
 	}
 
 	return true;
