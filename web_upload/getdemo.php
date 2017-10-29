f657c90eeb55415dcc7379b7b501710e613cbdd9
Kruzya <kruzefag@gmail.com>
2017-10-29 09:33:03 +0400

    Updated scripts for Export Bans and Demo file getting

--- web_upload/getdemo.php
+++ web_upload/getdemo.php
@@ -28,10 +28,10 @@
 require_once("init.php");
 
 if(!isset($_GET['id']) || !isset($_GET['type']))
-  die('No id or type parameter.');
+    die('No id or type parameter.');
 
 if(strcasecmp($_GET['type'], "U") != 0 && strcasecmp($_GET['type'], "B") != 0 && strcasecmp($_GET['type'], "S") != 0)
-  die('Bad type');
+    die('Bad type');
 
 $id = (int)$_GET['id'];
 
@@ -39,22 +39,21 @@ $demo = $GLOBALS['db']->GetRow("SELECT filename, origname FROM `".DB_PREFIX."_de
 //Official Fix: https://code.google.com/p/sourcebans/source/detail?r=165
 if(!$demo)
 {
-  die('Demo not found.');
+    die('Demo not found.');
 }
 
 if((!in_array($demo['filename'], scandir(SB_DEMOS)) || !file_exists(SB_DEMOS . "/" . $demo['filename'])) && $_GET['type'] != "U")
 {
-  die('File not found.');
+    die('File not found.');
 }
 
 if($_GET['type'] != "U"){
-$demo['filename'] = basename($demo['filename']);
-header('Content-type: application/force-download');
-header('Content-Transfer-Encoding: Binary');
-header('Content-disposition: attachment; filename="' . $demo['origname'] . '"');
-header("Content-Length: " . filesize(SB_DEMOS . "/" . $demo['filename']));
-readfile(SB_DEMOS . "/" . $demo['filename']);
+    $demo['filename'] = basename($demo['filename']);
+    header('Content-type: application/force-download');
+    header('Content-Transfer-Encoding: Binary');
+    header('Content-disposition: attachment; filename="' . $demo['origname'] . '"');
+    header("Content-Length: " . filesize(SB_DEMOS . "/" . $demo['filename']));
+    readfile(SB_DEMOS . "/" . $demo['filename']);
 }else{
-header( 'Location: '.$demo['origname'].'', true, 301 );
+    header( 'Location: '.$demo['origname'].'', true, 301 );
 }
-?>
