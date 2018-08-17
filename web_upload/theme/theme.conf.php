632605aec6d882be706e16435cba0f155afe1fa4
Kruzya <CrazyHackGUT@users.noreply.github.com>
2018-08-17 16:16:49 +0400

    Updated theme version

--- web_upload/theme/theme.conf.php
+++ web_upload/theme/theme.conf.php
@@ -32,7 +32,7 @@ define('theme_name', "SourceBans Theme (DeluxeEdition)");
 define('theme_author', "ASTRO & XaH JoB & Vampir & SAZONISCHE & NoTiCE & CrazyHackGUT & Серый™");
 
 // Set the version of the theme here
-define('theme_version', "1.1.5.4");
+define('theme_version', "1.1.6");
 
 // Set the link of the theme here
 define('theme_link', "https://discord.gg/BrTP9Cu");
@@ -40,4 +40,3 @@ define('theme_link', "https://discord.gg/BrTP9Cu");
 // Set the screenshot filename for your theme (must be inside your theme folder) 
 // Must be:  250px wide  X 170px High
 define('theme_screenshot', "");
-?>
