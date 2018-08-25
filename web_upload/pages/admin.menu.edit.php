dea0ac2cb0f746707229672b1ec6e601aea647a0
Kruzya <kruzefag@gmail.com>
2018-08-26 00:34:05 +0400

    Implemented new DB Manager, fix #171

--- web_upload/pages/admin.menu.edit.php
+++ web_upload/pages/admin.menu.edit.php
@@ -2,37 +2,58 @@
 if(!defined("IN_SB")){echo "Ошибка доступа!";die();}
 global $userbank, $theme;
 
-	if(!$userbank->HasAccess(ADMIN_OWNER))
-		CreateRedBox("Доступ запрещен!", "У вас нету доступных привилегий на просмотр данной страницы.");
-	else {
-	
-		if(isset($_POST['Link']))
-		{ 
-			if ($_POST['Link'] == "edit"){
-				$on_act = (isset($_POST['on_link']) && $_POST['on_link'] == "on" ? 1 : 0);
-				$system = $GLOBALS['db']->GetRow("SELECT url,system FROM `" . DB_PREFIX . "_menu` WHERE `id` = " . (int) $_GET['id']);
-				
-				$add = $GLOBALS['db']->Execute("UPDATE `" . DB_PREFIX . "_menu` SET `text` = ?, `description` = ?, `url` = ?, `system` = ?, `enabled` = ?, `priority` = ?, `newtab` = ? WHERE `id` = ?", array($_POST['names_link'], $_POST['des_link'], ((int) $system['system']!=0)?$_POST['url_link']:$system['url'], $system['system'], $on_act, $_POST['priora_link'], (($_POST['onNewTab']=="on")?"1":"0"), (int) $_GET['id']));
-				
-				PushScriptToExecuteAfterLoadPage(sprintf("setTimeout(function() { %s; }, 1350);", generateMsgBoxJS("Успех!", "Ссылка успешно сохранена!", "green", "", true)));
-				FatalRefresh("index.php?p=admin&c=menu");
-			}
-		}
-			
-			
-		$list_menu = $GLOBALS['db']->GetRow("SELECT * FROM ".DB_PREFIX."_menu WHERE id = '".(int)$_GET['id']."';");
-		if (count($list_menu) > 0) {
-			$theme->assign('text', $list_menu['text']);
-			$theme->assign('url', $list_menu['url']);
-			$theme->assign('des', $list_menu['description']);
-			$theme->assign('prior', $list_menu['priority']);
-			$theme->assign('enab', $list_menu['enabled']);
-			$theme->assign('system', ($list_menu['system']==1));
-			$theme->left_delimiter = "{";
-			$theme->right_delimiter = "}";
-			$theme->display('page_admin_menu_edit.tpl');
-			echo "<script>$('on_link').checked = ".(int)$list_menu['enabled'].";</script>";
-			echo "<script>$('onNewTab').checked = ".(int)$list_menu['newtab']."</script>";
-		}
-	}
-?>
+$DB = \DatabaseManager::GetConnection();
+
+if(!$userbank->HasAccess(ADMIN_OWNER))
+  CreateRedBox("Доступ запрещен!", "У вас нету доступных привилегий на просмотр данной страницы.");
+else {
+  if(isset($_POST['Link'])) { 
+    if ($_POST['Link'] == "edit"){
+      // insert.
+      $on_act = (isset($_POST['on_link']) && $_POST['on_link'] == "on" ? 1 : 0);
+
+      $DB->Prepare('UPDATE
+        `{{prefix}}menu`
+      SET
+        `text` = :text,
+        `description` = :description,
+        `url` = :url,
+        `enabled` = :enabled,
+        `priority` = :priority
+      WHERE
+        `id` = :id');
+      $DB->BindMultipleData([
+        'id'          => $_GET['id'],
+        'text'        => $_POST['names_link'],
+        'description' => $_POST['des_link'],
+        'url'         => $_POST['url_link'],
+        'enabled'     => $on_act,
+        'priority'    => $_POST['priora_link']
+      ]);
+      $DB->Finish();
+
+      PushScriptToExecuteAfterLoadPage(sprintf("setTimeout(function() { %s; }, 1350);", generateMsgBoxJS("Успех!", "Ссылка успешно сохранена!", "green", "", true)));
+      FatalRefresh("index.php?p=admin&c=menu");
+    }
+  }
+
+  $DB->Prepare('SELECT * FROM `{{prefix}}menu` WHERE `id` = :id');
+  $DB->BindData('id', $_GET['id']);
+  $Result = $DB->Finish();
+  $list_menu = $Result->Single();
+  $Result->EndData();
+
+  if (count($list_menu) > 0) {
+    $theme->assign('text', $list_menu['text']);
+    $theme->assign('url', $list_menu['url']);
+    $theme->assign('des', $list_menu['description']);
+    $theme->assign('prior', $list_menu['priority']);
+    $theme->assign('enab', $list_menu['enabled']);
+    $theme->assign('system', ($list_menu['system']==1));
+    $theme->left_delimiter = "{";
+    $theme->right_delimiter = "}";
+    $theme->display('page_admin_menu_edit.tpl');
+    echo "<script>$('on_link').checked = ".(int)$list_menu['enabled'].";</script>";
+    echo "<script>$('onNewTab').checked = ".(int)$list_menu['newtab']."</script>";
+  }
+}
