c104c2f6d797819dee83ab19c7ccb9cfd1795e0c
Kruzya <kruzefag@gmail.com>
2018-09-01 23:23:47 +0400

    Added support boot() method for classes

--- web_upload/includes/__loader.php
+++ web_upload/includes/__loader.php
@@ -4,10 +4,13 @@ function RegisterDirForAutoload($dir = NULL) {
     $dirname = dirname(__FILE__);
 
   spl_autoload_register(function($className) use ($dir) {
-    $className = str_replace('\\', '/', $className);
-    $path    = "$dir/$className.php";
+    $ClassPath = str_replace('\\', '/', $className);
+    $path      = "$dir/$ClassPath.php";
 
     if (file_exists($path))
       require_once($path);
+
+    if (is_callable([$className, 'boot']))
+      call_user_func_array([$className, 'boot'], []);
   });
 }
