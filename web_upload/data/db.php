0604d7344d90cfd0a6e76b2a753990f95be6866a
Kruzya <kruzefag@gmail.com>
2018-09-25 20:21:45 +0400

    Revert DB Config changes

--- web_upload/data/db.php
+++ web_upload/data/db.php
@@ -1,16 +1,14 @@
 <?php
-if (!defined('IN_SB')) exit();
-
+if (!defined('IN_SB'))
+  exit();
 /**
  * This file contains all database configurations for
  * using in SourceBans in new DB Framework.
  */
 \DatabaseManager::CreateConfig('SourceBans', [
-  'dsn'     => '',
-  'user'    => '',
-  'pass'    => '',
-  'prefix'  => '',
-  'options' => [
-    \PDO::ATTR_ERRMODE  => \PDO::ERRMODE_EXCEPTION
-  ]
+  'dsn'     => 'mysql:dbname=sourcebans;host=127.0.0.1;charset=UTF8',
+  'user'    => 'sourcebans',
+  'pass'    => 'password',
+  'prefix'  => 'sb_',
+  'options' => []
 ]);
