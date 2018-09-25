c6e1ca199c8b7052b58d9ca729924bd5a1fdcfed
Kruzya <kruzefag@gmail.com>
2018-09-25 20:23:25 +0400

    Removed all Smarty cache items, updated gitignore

--- web_upload/data/theme_c/TCache^%%6E^6EC^6ECAF77A%%content_header.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.29, created on 2018-09-18 17:04:04
-         compiled from content_header.tpl */ ?>
-<section id="content">
-	<div class="container">
-		<?php if ($this->_tpl_vars['xleb']): ?>
-			<ol class="card breadcrumb" id="breadcrumb" style="padding: 8px 20px;">
-			</ol>
-		<?php endif; ?>
