f7b306e554f042b175d6e65f4dcbb632d2178fc1
inzanty <53940590+inzanty@users.noreply.github.com>
2020-10-18 15:30:55 +0300

    drop old type of array and die() func

--- web_upload/pages/admin.detail.navbar.php
+++ web_upload/pages/admin.detail.navbar.php
@@ -25,20 +25,25 @@
 //
 // *************************************************************************
 
-if(!defined("IN_SB")){echo "Ошибка доступа!";die();} 
+if(!defined("IN_SB")){echo "Ошибка доступа!";exit();} 
 global $theme;
 
 $first = true; 
 $i=0;
-$tabs = array();
+$tabs = [];
+
 foreach($var AS $v)
 { 
 	if(empty($v['title']))
 	{
 		$i++; continue;
 	} 
+
 	if($first) 
+        {
 		$GLOBALS['enable'] = $v['id']; 
+        }
+
 	if(isset($v['external']) && $v['external'] == true) 
 	{
 		$lnk = $v['url']; 
@@ -49,23 +54,24 @@ foreach($var AS $v)
 		$lnk = "#^" . $v['id']; 
 		$click = "SwapPane(". $v['id'] .");";
 	} 
+
 	if($i == 0) 
+        {
 		$class = "active"; 
+        }
 	else 
+        {
 		$class = "";
-	$itm = array();
+        }
+
+	$itm = [];
 	$itm['tab'] = "<li id='tab-". $v['id'] . "' class='" . $class . "'><a href='$lnk' id='admin_tab_".$v['id']."' onclick=\"$click\"> " . $v['title'] . "</a></li>";
-	array_push($tabs, $itm) ;
+	array_push($tabs, $itm);
 	$i++;
 	$first=false;
 }
 
-//if($_GET['p'] == "account")
-	//$theme->assign('pane_image','<img src="themes/' . SB_THEME . '/images/admin/your_account.png"> </div>') ;
-//else 
-	//$theme->assign('pane_image', '<img src="themes/' . SB_THEME . '/images/admin/'.  $_GET['c'] . '.png"> </div>');
 	
 $theme->assign('tabs', $tabs);
-
 $theme->display('item_admin_tabs.tpl');
 ?>
