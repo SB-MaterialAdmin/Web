9e1dd90e1b6c5557103e7e81f6540802442c051f
inzanty <53940590+inzanty@users.noreply.github.com>
2020-10-18 11:11:53 +0300

    change else if to switch statement && die() to exit()

--- web_upload/exportbans.php
+++ web_upload/exportbans.php
@@ -4,26 +4,32 @@ include_once('init.php');
 $type = filterInput(INPUT_GET, 'type', FILTER_SANITIZE_STRING);
 
 if (!$userbank->HasAccess(ADMIN_OWNER) && !$GLOBALS['config']['config.exportpublic'])
-  die('У Вас нет доступа к данной функции.');
+  exit('У Вас нет доступа к данной функции.');
 else if (is_null($type))
-	die('Используйте только ссылки в самой системе!');
+	exit('Используйте только ссылки в самой системе!');
 
 $cmd    = '';
 $file   = '';
 $type   = 0;
 $column = '';
-if ($type == 'steam') {
-  $cmd    = 'banid';
-  $file   = 'banned_user.cfg';
-  $type   = 0;
-  $column = 'authid';
-} elseif ($type == 'ip') {
-  $cmd    = 'addip';
-  $file   = 'banned_ip.cfg';
-  $type   = 1;
-  $column = 'ip';
-} else
-  die("Unknown type $type");
+
+switch ($type)
+{
+    case "steam":
+       $cmd    = 'banid';
+       $file   = 'banned_user.cfg';
+       $type   = 0;
+       $column = 'authid';
+       break;
+    case "ip":
+       $cmd    = 'addip';
+       $file   = 'banned_ip.cfg';
+       $type   = 1;
+       $column = 'ip';
+       break;
+    default:
+       exit("Unknown type $type");
+}
 
 $DB = \DatabaseManager::GetConnection();
 $Result = $DB->Query("SELECT $column AS id FROM `{{prefix}}bans` WHERE `length` = 0 AND `RemoveType` IS NULL AND `type` = $type");
